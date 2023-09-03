cb05dc98a011dc28a93b9a378493e9ce0cf7a8da
kokk <kokk7310@gmail.com>
2023-09-03 10:50:32 +0900

    axios 인터셉터 없는 api 추가

--- src/shared/api/index.js
+++ src/shared/api/index.js
@@ -6,14 +6,18 @@ export const api = axios.create({
   baseURL: process.env.REACT_APP_MAIN_SERVER,
 });
 
+export const generalApi = axios.create({
+  baseURL:process.env.REACT_APP_MAIN_SERVER,
+})
 
+/* 엑세스 토큰 담아서 보내는 axios */
 api.interceptors.request.use(async config =>{
   /*config.headers['content-type'] = 'application/jso; charset=utf-8';
   config.headers['X-Requested-With'] = 'XMLHttpRequest';
   config.headers.Accept = '*!/!*';*/
 
-  config.headers.access_token = cookies.get("accessToken");
-  config.headers.refresh_token = cookies.get("refreshToken");
+  config.headers.accessToken = cookies.get("accessToken");
+  config.headers.refreshToken = cookies.get("refreshToken");
   return config;
 });
 
@@ -22,7 +26,7 @@ api.interceptors.response.use(
     return response;
   },
   (error) => {
-    if(error.response.status === 401) {
+    if(error.response.data.statusCode === 401) {
       deleteCookie("accessToken");
       alert("로그인이 만료되었습니다.")
       window.location.href = "localhost:3000"
@@ -31,4 +35,5 @@ api.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
 
